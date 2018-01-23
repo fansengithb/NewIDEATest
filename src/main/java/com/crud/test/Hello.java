b3d56fe5c06d5f3240ee554e0fa78e87c7c9f5c7
fansen <1297264972@qq.com>
2018-01-23 22:26:14 +0800

    test

---
+++ src/main/java/com/crud/test/Hello.java
@@ -0,0 +1,10 @@
+package com.crud.test;
+
+/**
+ * Created by fansen on 2018/1/23.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hell git from IDEA!");
+    }
+}
