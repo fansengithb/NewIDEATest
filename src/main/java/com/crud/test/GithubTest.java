a2e167af0f8d06610be417dbcab66bf22d528363
fansen <1297264972@qq.com>
2018-01-23 22:42:48 +0800

    test

--- src/main/java/com/crud/test/GithubTest.java
+++ src/main/java/com/crud/test/GithubTest.java
@@ -5,6 +5,6 @@ package com.crud.test;
  */
 public class GithubTest {
     public static void main(String[] args) {
-        System.out.println("-------------------");
+        System.out.println("---------wwwweee----------");
     }
 }
