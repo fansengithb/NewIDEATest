26ccdfeab508db14c23b2fdb1fc2c1a39c46bbb7
fansen <1297264972@qq.com>
2018-01-25 23:29:00 +0800

    17--00

--- src/main/java/com/crud/controller/EmployeeController.java
+++ src/main/java/com/crud/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.crud.controller;
 
 import com.crud.bean.Employee;
+import com.crud.bean.Msg;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import  com.crud.service.EmployeeService;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -27,7 +29,34 @@ public class EmployeeController {
      * @return
      */
 
+
+
+    /**
+     * 导入jackson包。
+     * @param pn
+     * @return
+     */
     @RequestMapping("/emps")
+    @ResponseBody
+    public Msg getEmpsWithJson(
+            @RequestParam(value = "pn", defaultValue = "1") Integer pn) {
+        //        引入pageHelper分页插件
+        PageHelper.startPage(pn,10);
+//        startPage后面紧跟的就是分页查询
+        List<Employee> emps = employeeService.getAll();
+//        使用pageInfo包装查询结果,将pageInfo交给页面
+//        包括详细的分页信息，包括查询的数据  传入连续显示的页数
+        PageInfo page = new PageInfo(emps,5);
+        return  Msg.success().add("pageInfo",page);
+
+    }
+
+
+
+
+
+
+//    @RequestMapping("/emps")
     public String getEmps(
             @RequestParam(value = "pn", defaultValue = "1") Integer pn,
             Model model){
