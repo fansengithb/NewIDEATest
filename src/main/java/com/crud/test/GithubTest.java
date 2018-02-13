f87ee6cd5455eb83f04398efe6ceddf956f4af16
fansengithb <1297264972@qq.com>
2018-02-13 22:30:16 +0800

    test

--- src/main/java/com/crud/test/GithubTest.java
+++ src/main/java/com/crud/test/GithubTest.java
@@ -1,10 +1,60 @@
 package com.crud.test;
 
+import org.apache.commons.lang3.time.DateFormatUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  * Created by fansen on 2018/1/23.
  */
-public class GithubTest {
-    public static void main(String[] args) {
-        System.out.println("---------wwwweee----------");
+public class GithubTest  {
+    public static void main(String[] args) throws  Exception {
+//        System.out.println("---------wwwweee----------");
+
+        test();
+    }
+
+
+
+    public  static  void  test9() {
+        System.out.println("$$$$$");
+        System.out.println(new Date().getTime());
+        System.out.println(new Date().getTime() % 100000000);
+        System.out.println((int) new Date().getTime() % 100000000);
+    }
+
+
+    /**
+     *    获取本周周一  00:00:00
+     */
+
+    public static void test()throws  Exception{
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        int day1 = calendar.get(Calendar.DAY_OF_WEEK)-1;
+        System.out.println("today:"+day1);
+
+
+
+        Calendar c = Calendar.getInstance();
+        c.set( Calendar.DATE, c.get( Calendar.DATE ) -(calendar.get(Calendar.DAY_OF_WEEK)-1-1 ));
+        Date day = c.getTime();
+        //  数据库查询的开始时间
+        Date start_time = sdf.parse(DateFormatUtils.format(day, "yyyy-MM-dd 00:00:00"));
+        System.out.println("monday---"+start_time);
+
+
+        c.set( Calendar.DATE, c.get( Calendar.DATE ) -5);
+        day = c.getTime();
+//        过去6天
+        System.out.println("last--day:"+sdf.parse(DateFormatUtils.format(day, "yyyy-MM-dd 00:00:00")));
+
+
     }
 }
+
+
--- src/main/java/com/crud/test/HelloTest.java
+++ src/main/java/com/crud/test/HelloTest.java
@@ -1,10 +1,41 @@
 package com.crud.test;
 
+import org.apache.commons.lang3.time.DateFormatUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 /**
  * Created by fansen on 2018/1/23.
  */
 public class HelloTest {
-    public static void main(String[] args) {
-        System.out.println("  hello,git!");
+    public static void main(String[] args) throws  Exception{
+        test1();
+//        System.out.println("  hello,git!");
+    }
+
+
+    public  static void  test()throws  Exception {
+        //            今天时间最后一秒
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//            数据库查询的结束时间
+        Date end_time = sdf.parse(DateFormatUtils.format(new Date(), "yyyy-MM-dd 23:59:59"));
+        Calendar c = Calendar.getInstance();
+        c.set( Calendar.DATE, c.get( Calendar.DATE ) - 7);
+        Date day = c.getTime();
+        //  数据库查询的开始时间
+        Date start_time = sdf.parse(DateFormatUtils.format(day, "yyyy-MM-dd 00:00:00"));
+
+        System.out.println("-- "+start_time);
+        System.out.println("-- "+end_time);
+
+    }
+    public  static void  test1()throws  Exception {
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.setTime(new Date());
+        System.out.println(      calendar.get(Calendar.DAY_OF_WEEK)-1);
+
     }
 }
