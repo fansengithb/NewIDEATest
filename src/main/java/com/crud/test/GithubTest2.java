65dd1f52f8f2ab12e96221923f0c5a2363c2de38
fansengithb <1297264972@qq.com>
2018-03-19 12:30:38 +0800

    test

---
+++ src/main/java/com/crud/test/GithubTest2.java
@@ -0,0 +1,10 @@
+package com.crud.test;
+
+/**
+ * Created by Dice on 2018/3/19.
+ */
+public class GithubTest2 {
+    public static void main(String[] args) {
+        System.out.println("hello   !");
+    }
+}
