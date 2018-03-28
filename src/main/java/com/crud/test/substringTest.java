1ca43bcef61ce849c0443d0b3df11b13e63abdcc
fansengithb <1297264972@qq.com>
2018-03-28 12:31:38 +0800

    test

---
+++ src/main/java/com/crud/test/substringTest.java
@@ -0,0 +1,10 @@
+package com.crud.test;
+
+public class substringTest {
+    public static void main(String[] args) {
+        String test = "abc|";
+        System.out.println("wwww");
+
+        System.out.println("---"+test.substring(0,test.length()-1));
+    }
+}
